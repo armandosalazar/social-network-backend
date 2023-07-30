098a5dbf49740fcf0356f83dd8d54052c64c02fd
Armando Salazar <armandosalazar351@gmail.com>
2023-07-30 01:46:23 -0600

    feat: upload picture profile found

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const multer = require('multer');
+
 const routes = require('./routes');
 
 const app = express();
@@ -13,6 +15,22 @@ app.use(cors()); // permite que cualquier cliente se conecte a la api
 app.use(morgan('dev'));
 
 app.use('/api', routes);
+
+// disk storage
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    }
+});
+// upload profile picture
+const upload = multer({ storage });
+
+app.post('/profile/picture', upload.single('picture'), (req, res) => {
+    res.send('Profile picture uploaded');
+});
 
 app.get('/ping', (req, res) => {
     res.send('pong');
