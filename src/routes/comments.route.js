329b7ba18476122c810e99940a81ac538188f571
Armando Salazar <armandosalazar351@gmail.com>
2023-07-31 23:19:41 -0600

    feat: add verify token middleware

--- src/routes/comments.route.js
+++ src/routes/comments.route.js
@@ -30,6 +30,36 @@ const comments = [
     username: 'Jane',
     content: 'Bye',
   },
+  {
+    id: 1,
+    username: 'John',
+    content: 'Hello World',
+  },
+  {
+    id: 2,
+    username: 'Jane',
+    content: 'Hi, John',
+  },
+  {
+    id: 3,
+    username: 'John',
+    content: 'How are you?',
+  },
+  {
+    id: 4,
+    username: 'Jane',
+    content: 'I am fine',
+  },
+  {
+    id: 5,
+    username: 'John',
+    content: 'Good to hear that',
+  },
+  {
+    id: 6,
+    username: 'Jane',
+    content: 'Bye',
+  },
 ];
 
 const router = Router();
