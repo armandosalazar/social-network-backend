607b415a61f1bdb2fab9f7a7b0366364a9a71f6d
Armando Salazar <armandosalazar351@gmail.com>
2023-08-07 02:07:34 -0600

    chore: hide personal user on users get

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
+const { Op } = require('sequelize');
 
 const verifyToken = require('../middleware/verifyToken.middleware');
 const { User } = require('../models');
 
 router.get('/', [verifyToken], async function (req, res) {
   const users = await User.findAll({
+    where: {
+      id: {
+        [Op.ne]: req.user.id,
+      },
+    },
     attributes: {
       exclude: ['password'],
     },
